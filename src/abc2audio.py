886e131246713d08fc7465db01c6034969d93b0d
appbird <project.appbird@gmail.com>
2024-06-21 21:15:47 +0900

    fix bug of abc2wav

--- src/abc2audio.py
+++ src/abc2audio.py
@@ -5,7 +5,10 @@ from Result import Result, ProcessState, ResultOK
 
 
 def write_midi_from_abc(abc_file_path:str, midi_file_path:str) -> Result:
-    sbp = subprocess.Popen(["abc2midi", abc_file_path, '-o', midi_file_path], stdout=subprocess.PIPE)
+    sbp = subprocess.Popen(["abc2midi", abc_file_path, '-o', midi_file_path],
+        stdout=subprocess.PIPE,
+        cwd=Path.cwd()
+    )
     out, _ = sbp.communicate()
     stdout_content = out.decode('utf-8')
     if any([line.startswith("Error") for line in stdout_content.splitlines()]): return Result(ProcessState.FAILED_ABC2MIDI, stdout_content)
@@ -14,7 +17,7 @@ def write_midi_from_abc(abc_file_path:str, midi_file_path:str) -> Result:
 
 
 def write_wav_from_midi(midi_file_path:str, wav_file_path:str, gain:float) -> Result:
-    sound_font_path = "sf2/GeneralUser_GS_1.471/GeneralUser_GS_v1.471.sf2"
+    sound_font_path = str(Path.cwd()/"sf2/GeneralUser_GS_1.471/GeneralUser_GS_v1.471.sf2")
     sbp = subprocess.Popen([
             "fluidsynth",
             # Render MIDI file to raw audio data and store in [file]
@@ -26,7 +29,8 @@ def write_wav_from_midi(midi_file_path:str, wav_file_path:str, gain:float) -> Re
             sound_font_path,
             midi_file_path,
         ],
-        stderr=subprocess.PIPE
+        stderr=subprocess.PIPE,
+        cwd=Path.cwd()
     )
     _, err = sbp.communicate()
     return ResultOK() if sbp.returncode == 0 else Result(ProcessState.FAILED_MIDI2WAV, err.decode('utf-8'))
@@ -41,4 +45,5 @@ def abc2wav(input_abc_path:str, midi_file:str, wav_file:str) -> Result:
 
     return result2 if len(result1.reason) == 0 else Result(ProcessState.OK, result1.reason + result2.reason)
 
-
+if __name__ == "__main__":
+    print(abc2wav(sys.argv[1], "a.midi", "a.wav").reason)
